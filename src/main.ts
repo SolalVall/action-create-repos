7fcfa3fb688c3bdcc854b05957a398532262347e
Zyker <solal.vallee@laposte.net>
2023-04-12 23:37:28 +0000

    [UPDATE] Make act work in codespace

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as github from '@actions/github'
 (async function main() {
     try {
         const repo: string = core.getInput('repo_name')
-        core.info('Create new action named ${repo}');
+        core.info(`Createe new action named ${repo}`);
     } catch (error) {
         core.setFailed((<any>error).message);
     }
