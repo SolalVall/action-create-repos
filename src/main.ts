a6b9e6787ce8e082d9f46f40a97129a7ab233c06
Zyker <solal.vallee@laposte.net>
2023-04-14 22:18:48 +0000

    [UPDATE] Add ability to choose from a template

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import * as core from '@actions/core'
-import * as github from '@actions/github'
-import { verifyInput, verifyRepository } from './verify'
+import { getOctokit, context } from '@actions/github'
+import { verifyInput, verifyRepository, verifyTemplateRepo } from './verify'
 
 // Skeleton
 ;(async function main() {
   try {
+    const templateRepository: string = 'repository-templates'
+
     // Verification steps
-    core.info('Verifying input(s) provided..')
     const repo: string = verifyInput('repo_input', core.getInput('repo_name'))
-    core.info('Verifying repository details')
-    verifyRepository(repo, core.getInput('github_token'))
-    core.info(`Createe new action named ${repo}`)
+    await verifyRepository(repo, core.getInput('github_token'))
+    await verifyTemplateRepo(templateRepository, core.getInput('github_token'))
   } catch (error) {
     core.setFailed((<any>error).message)
   }
--- src/verify.ts
+++ src/verify.ts
@@ -1,4 +1,4 @@
-import { getOctokit } from '@actions/github'
+import { getOctokit, context } from '@actions/github'
 import * as core from '@actions/core'
 
 // Look for inputs provided by user via workflow_dispatch event
@@ -12,37 +12,67 @@ export function verifyInput(inputType: string, inputValue: string): string {
     }
   }
 
+  core.info('[OK] Repository name')
   return inputValue
 }
 
 export async function verifyRepository(repoName: string, token: string) {
   const octokit = getOctokit(token)
+  const repoOwner = context.repo.owner
 
   try {
     await octokit.rest.repos
       .get({
-        owner: 'SolalVall',
+        owner: repoOwner,
         repo: repoName
       })
       .then((res) => {
-        // If promise is successfull it means the repository exists. So let's make the user aware
+        // If request is successfull it means the repository exists. So let's make the user aware
         throw new Error(
-          `Repository name provided (${repoName}) already exists in`
+          `Repository name provided (${repoName}) already exists in ${repoOwner}`
         )
       })
       .catch((err) => {
-        core.info('heyy')
         core.debug(`Response body: ${err}`)
-        // We continue only when the repository does not exists
-        if (err.status == 404) {
-          return
-        } else {
+        core.debug(`Response status: ${err.status}`)
+        // We continue only when the repository does not exists. <=> 404 - Not found
+        if (err.status != 404) {
           throw err
         }
       })
   } catch (err) {
     if (err instanceof Error) {
-      core.error(err.message)
+      throw new Error(err.message)
     }
   }
+
+  core.info('[OK] Repository space available')
+}
+
+export async function verifyTemplateRepo(
+  templateRepoName: string,
+  token: string
+) {
+  const octokit = getOctokit(token)
+  const repoOwner = context.repo.owner
+
+  await octokit.rest.repos
+    .get({
+      owner: repoOwner,
+      repo: templateRepoName
+    })
+    .then((res) => {
+      core.info('[OK] Repository containing templates found')
+      return
+    })
+    .catch((err) => {
+      core.debug(`Response body: ${err}`)
+      core.debug(`Response status: ${err.status}`)
+      // Template repo must exists for action to be successfull!
+      if (err.status == 404) {
+        throw new Error(
+          `Template repository (${templateRepoName}) does not exists in ${repoOwner}. Please create it before continue!`
+        )
+      }
+    })
 }
